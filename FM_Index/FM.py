3c9304d3eb1a8f325b7f5e83547b190b158e5e2f
Carsen Ball <ballcarsen@gmail.com>
2017-10-30 12:49:27 -0600

    "I think its done"

--- FM_Index/FM.py
+++ FM_Index/FM.py
@@ -2,11 +2,14 @@ class FM:
     def __init__(self,string):
         self.string = string
         self.BW = None
+        #The index in the string at which the corresponding suffix to the Burrows Wheeler exists
+        self.saI = None
         self.c = None
     #Creates an array that hold the BW info
     def bw(self):
         self.BW = []
         substrings = []
+        self.saI  = []
         for i in range(len(self.string)):
             substrings.append(self.string[i:] + str(i))
         #Sorts suffixs by lexigraphical order
@@ -35,7 +38,7 @@ class FM:
         print(self.BW)
         for k in substrings:
             ind = int(k[len(k) - 1])
-
+            self.saI.append(ind)
             self.BW.append(self.string[ind - 1])
         print(self.BW)
     #Creates a dicitonary that holds the C values for each char in the string
@@ -50,7 +53,7 @@ class FM:
             if temp[k] != char:
                 self.c[temp[k]] = k
                 char = temp[k]
-    #Returns the number of occurences of a specified char in the specified range.
+    #Returns the number of occurences of a specified char in the specified range of the BW.
     def occ(self,char,index):
         sub = self.BW[0:index]
         print(self.BW)
@@ -58,12 +61,26 @@ class FM:
         return(sub.count(char))
     #backwards pattern matching
     def backwards(self,pattern):
-        sp = self.c[pattern[len(pattern - 1)]]
-        print(sp)
-        ep = 0
-        range = (0,0)
+        sp = self.c[pattern[len(pattern ) - 1]]
+        #what happens if it is the last lexogrpahical char?
+        ep = self.c[self.findNextChar(pattern[len(pattern ) - 1])]
+        for k in range(len(pattern) - 1):
+            print(k)
+            print(len(pattern))
+            char = pattern[len(pattern) - k - 2]
+            sp = self.c[char] + self.occ(char, sp - 1)  + 1
+            ep = self.c[char] + self.occ(char, ep)
+        print(sp, ep)
 
-        #for i in range(len(pattern)):
+    def findNextChar(self,char):
+        exists  = False
+        count = 1
+        while not exists:
+            if chr((ord(char) + count)) in self.c:
+                return chr((ord(char)+count))
+                exists = True
+            else:
+                count += 1
 
 
 
@@ -72,4 +89,4 @@ if 'name == __main__' :
     fm1 = FM("acgtcga$")
     fm1.bw()
     fm1.char_count()
-    fm1.backwards('ac')
+    fm1.backwards('cg')
